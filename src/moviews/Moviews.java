4b1b813a3a381820a6ff74ef83fe03de191302ba
ahsanatha <ahsan.athallah@gmail.com>
2018-11-01 00:45:31 +0700

    init

---
+++ src/moviews/Moviews.java
@@ -0,0 +1,13 @@
+package moviews;
+
+/**
+ *
+ * @author Muhammad Ahsan Athallah & Muhammad Zikri Syahbani
+ */
+public class Moviews {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
