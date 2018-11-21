98ba951fdd3d111e613afd26308720e9aa49b4b5
ahsanatha <ahsan.athallah@gmail.com>
2018-11-21 19:01:27 +0700

    update

--- src/org/Moviews/Main/Moviews.java
+++ src/org/Moviews/Main/Moviews.java
@@ -1,13 +1,11 @@
-package moviews;
-
+package org.Moviews.Main;
 /**
- *
  * @author Muhammad Ahsan Athallah & Muhammad Zikri Syahbani
  */
 public class Moviews {
 
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+
     
+    }
 }
---
+++ src/org/Moviews/Model/Connect.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.Moviews.Model;
+
+/**
+ *
+ * @author TSR
+ */
+public class Connect {
+   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+   static final String DB_URL = "jdbc:mysql://localhost/Moviewsdb";
+}
