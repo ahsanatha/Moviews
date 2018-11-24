7ec8d280f564686e8f30212295ee7371e7c36bbb
zikriss <45308013+zikriss@users.noreply.github.com>
2018-11-24 16:14:34 +0700

    atha

--- src/org/Moviews/Main/Moviews.java
+++ src/org/Moviews/Main/Moviews.java
@@ -3,9 +3,10 @@ package org.Moviews.Main;
  * @author Muhammad Ahsan Athallah & Muhammad Zikri Syahbani
  */
 public class Moviews {
+    
 
     public static void main(String[] args) {
-
-    
+        String atha = "atha";
+        System.out.println("namanya "+atha);
     }
 }
